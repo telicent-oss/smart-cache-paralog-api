432a89a6d02d1e912f065e4fef149c6316eb8c59
bhavi <138500484+bpatelicent@users.noreply.github.com>
2025-03-17 12:06:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ __license__ = """
 Copyright (c) Telicent Ltd.
 
 This software is licensed under the terms found in the LICENSE file of this resository. 
-    http://www.apache.org/licenses/LICENSE-2.0
 
 The above copyright notice and license conditions shall be included in all
 copies or substantial portions of the Software.
